5131df2479ee7f5ea4f7a454498d5c732f07cea2
likewagon <likewagon@outlook.com>
2021-06-07 15:34:40 +0800

    add css files and web3provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import '../styles/font.css'
+import "../styles/globals.css";
+import "../styles/font.css";
+import "../styles/color.css";
+import "read-more-less-react/dist/index.css";
+
+import Web3 from "web3"
+import Web3Provider from "web3-react";
+import connectors from "libs/web3-connectors";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Web3Provider
+      connectors={connectors}
+      libraryName={'web3.js'}
+      web3Api={Web3}
+    >
+      <Component {...pageProps} />
+    </Web3Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
