4067134f8fc41a2cdeddcbf73c91ab0cbe3294e2
likewagon <likewagon@outlook.com>
2021-06-07 15:33:23 +0800

    add global opensea object

---
+++ libs/opensea-js.ts
@@ -0,0 +1,21 @@
+import * as Web3 from 'web3'
+import {OpenSeaPort, Network} from 'opensea-js'
+
+interface CustomNodeJsGlobal extends NodeJS.Global {
+  seaport: OpenSeaPort;
+}
+
+//prevent multiple instances of Prisma Client in development
+declare const global: CustomNodeJsGlobal;
+
+const provider = new Web3.providers.HttpProvider('https://mainnet.infura.io')
+
+const seaport = global.seaport || new OpenSeaPort(provider, {
+  networkName: Network.Main
+});
+
+if(process.env.NODE_ENV === 'development'){
+  global.seaport = seaport;
+} 
+
+export default seaport
